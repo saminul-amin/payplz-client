16edd1655b067b1ee5454faee99f52f473f50728
Md. Saminul Amin <saminul.amin@gmail.com>
2025-02-19 18:49:01 +0600

    margin updated

--- src/pages/shared/Profile.jsx
+++ src/pages/shared/Profile.jsx
@@ -1,7 +1,7 @@
 export default function Profile() {
   return (
     <div>
-      <h2 className="text-3xl font-semibold text-center mt-12">
+      <h2 className="text-3xl font-semibold text-center mt-24">
         Profile will be updated later!
       </h2>
     </div>
